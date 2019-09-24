b162ce21b02fced2229734e7175b42037225d502
Kunal Singhal <kunal.singhal19@gmail.com>
2019-09-24 13:09:56 +0530

    task3

--- src/main/java/com/stackroute/Main2.java
+++ src/main/java/com/stackroute/Main2.java
@@ -8,10 +8,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main2 {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie m=(Movie) context.getBean("movie1");
+        Movie m=(Movie) context.getBean("movie");
         System.out.println(m.getActor().getName());
-        Movie m2=(Movie) context.getBean("movie2");
-        System.out.println(m==m2);
+//        Movie m2=(Movie) context.getBean("movie2");
+//        System.out.println(m==m2);
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+        Movie m1=(Movie) context1.getBean("movie1");
+        System.out.println(m1.getActor().getName());
 
 //       Actor m1=(Actor) context.getBean("actor1");
 //      System.out.println(m1.getName());
